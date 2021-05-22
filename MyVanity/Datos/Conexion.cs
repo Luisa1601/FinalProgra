3fd913fa8b25e66881969daa1ffc7fd0e37f934a
Luisa1601 <80083060+Luisa1601@users.noreply.github.com>
2021-05-21 18:47:56 -0600

    crear conexion mysql

--- MyVanity/Datos/Conexion.cs
+++ MyVanity/Datos/Conexion.cs
@@ -1,1 +1,30 @@
-﻿
+﻿using MySql.Data.MySqlClient;
+using System;
+
+namespace MyVanity.Datos
+{
+    class Conexion
+    {
+        private static string server = "localhost";
+        private static string database = "bd_vanity";
+        private static string userId = "root";
+        private static string pass = "";
+        public static MySqlConnection conexion()
+        {
+            string cadenaConexion = "Database=" + database +"; Data Source=" + server + "; User Id=" + userId + "; Password=" + pass + "; Convert Zero Datetime=True";
+
+            try
+            {
+                MySqlConnection connection = new MySqlConnection(cadenaConexion);
+                return connection;
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+
+        }
+        
+    }
+}
